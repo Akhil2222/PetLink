835f8026295cc06b64eb813d18e0ca5c19ff7de2
sgangwar17 <sandali.gangwar@gmail.com>
2026-04-14 16:16:57 -0500

    Installed CT

--- PetLink/ViewModels/MainViewModel.cs
+++ PetLink/ViewModels/MainViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Android.Text.Style;
 using AndroidX.RecyclerView.Widget;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.VisualStudio.PlatformUI;
 using PetLink.Models.Titles;
 using System;
 using System.Collections.Generic;
@@ -7,7 +10,7 @@ using System.Text;
 
 namespace PetLink.ViewModels
 {
-    public partial class MainViewModel : ObservableObject
+    public partial class MainViewModel : CommunityToolkit.Mvvm.ComponentModel.ObservableObject
     {
         //public string Layouts { get; set; } = TitleMain.Layouts;
         //public string Images { get; set; } = TitleMain.Images;
@@ -31,28 +34,12 @@ namespace PetLink.ViewModels
         //    await Application.Current.MainPage.Navigation.PushAsync(new LayoutsPage());
         //}
 
-        [RelayCommand]
-        private async Task ImagesClicked()
-        {
-            await Shell.Current.GoToAsync(nameof(ImagesPage));
-        }
-
-        [RelayCommand]
-        private async Task LayoutsClicked()
-        {
-            await Shell.Current.GoToAsync(nameof(LayoutsPage));
-        }
-
-        [RelayCommand]
-        private async Task CollectionsClicked()
-        {
-            await Shell.Current.GoToAsync(nameof(CollectionsPage));
-        }
+        
 
         [RelayCommand]
         private async Task ControlsClicked()
         {
-            await Shell.Current.GoToAsync(nameof(ControlsPage));
+            await Shell.Current.GoToAsync(nameof(MainPage));
         }
 
         public MainViewModel()

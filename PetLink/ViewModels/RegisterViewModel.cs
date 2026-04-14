b095703989d7b0ac1cea71590f3f20ca2f2a7162
agupta234 <gupta.anya234@gmail.com>
2026-04-14 16:07:22 -0500

    register pages made

---
+++ PetLink/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Android.Database;
+using PetLink.Models.Titles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetLink.ViewModels
+{
+    public partial class RegisterViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = TitleRegister.Title;
+    }
+}

0c7328b4527c91d1820b8ab42d6197f7868d817b
sgangwar17 <sandali.gangwar@gmail.com>
2026-04-16 15:55:10 -0500

    Trial

--- PetLink/Models/Titles/TitleMain.cs
+++ PetLink/Models/Titles/TitleMain.cs
@@ -8,7 +8,7 @@ namespace PetLink.Models.Titles
 {
     public static class TitleMain
     {
-        public static string Title { get; } = "PetLink";
+        public static string Title { get; } = "Welcome To PetLink";
         public static string Login { get; } = "Login";
         public static string Register { get; } = "Register";
 

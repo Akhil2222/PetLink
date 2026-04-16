d43d446a5030f6e11b2f6629aeab6beb84b17b2d
AnanyaVelmurugan <ananya.velmurugan@gmail.com>
2026-04-16 16:06:05 -0500

    Worked on Login XAML

--- PetLink/Models/Titles/TitleLogin.cs
+++ PetLink/Models/Titles/TitleLogin.cs
@@ -2,10 +2,12 @@
 {
     public static class TitleLogin
     {
-        public static string Title { get; } = "";
+        public static string Title { get; } = "Login to PetLink";
         public static string UsernameText { get; } = "Enter your username (your email)";
         public static string PasswordText { get; } = "Enter your password";
+        public static string Submit { get; } = "Submit";
 
 
     }
 }
+

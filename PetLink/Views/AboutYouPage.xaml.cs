f3db28ce19ed474416158937d14bffdc2814403d
epiphany264 <xuannikka@gmail.com>
2026-04-16 15:32:05 -0500

    about you page update 3

--- PetLink/Views/AboutYouPage.xaml.cs
+++ PetLink/Views/AboutYouPage.xaml.cs
@@ -11,7 +11,7 @@ public AboutYouPage()
 
 private async void SubmitClicked(object sender, EventArgs e)
 {
-	string entryText = EntryValue.Text;
+	string entryText = EntryValuePhone.Text;
 
 	if (string.IsNullOrEmpty(entryText))
 	{

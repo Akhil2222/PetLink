d4d610379198af0ceee6996bdb6489615f8600e9
sgangwar17 <sandali.gangwar@gmail.com>
2026-04-16 15:26:08 -0500

    Pulling to retrieve changes

--- PetLink/ViewModels/MainViewModel.cs
+++ PetLink/ViewModels/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using CommunityToolkit.Mvvm;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Microsoft.VisualStudio.PlatformUI;
 using PetLink.Models.Titles;
 using System;
 using System.Collections.Generic;
@@ -19,21 +18,6 @@ namespace PetLink.ViewModels
 
         [ObservableProperty]
         private string title = TitleMain.Title;
-
-        //public ICommand OnLayoutsClicked { get; set; }
-        //public MainViewModel()
-        //{
-        //    Title = TitleMain.Title;
-
-        //    OnLayoutsClicked = new Command(OnLayoutsClickedAsync);
-        //}
-
-        //private async void OnLayoutsClickedAsync()
-        //{
-        //    await Application.Current.MainPage.Navigation.PushAsync(new LayoutsPage());
-        //}
-
-        
 
         [RelayCommand]
         private async Task ControlsClicked()

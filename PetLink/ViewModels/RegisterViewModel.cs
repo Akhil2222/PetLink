377bc1868f501f3351de9d6391f6442d4b72fdd2
Akhil2222 <akhily98765@gmail.com>
2026-04-16 15:28:26 -0500

    Adjust MVVM to MPVM

--- PetLink/ViewModels/RegisterViewModel.cs
+++ PetLink/ViewModels/RegisterViewModel.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using CommunityToolkit.Mvvm.Input;
-using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace PetLink.ViewModels
 {

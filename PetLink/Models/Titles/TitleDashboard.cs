1a35dc2cc1cc538d592847822ec77339fe106b75
sgangwar17 <sandali.gangwar@gmail.com>
2026-04-20 15:53:59 -0500

    HIIII

--- PetLink/Models/Titles/TitleDashboard.cs
+++ PetLink/Models/Titles/TitleDashboard.cs
@@ -8,7 +8,7 @@ namespace PetLink.Models.Titles
 {
     public static class TitleDashboard
     {
-        public static string Title { get; } = "Welcome To PetLink";
+        public static string Title { get; } = "Your PetLink Dashboard";
         public static string Profile { get; } = "Profile";
         public static string FindMyPet { get; } = "Find My Pet";
         public static string LikedPets { get; } = "Liked Pets";
